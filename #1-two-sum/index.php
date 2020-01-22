16033e83d4c2fb99feb630a960bb53529fb27668
William Trindade <williamtrindade777@gmail.com>
2020-01-21 23:49:59 -0300

    Create index.php

---
+++ #1-two-sum/index.php
@@ -0,0 +1,23 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $array_size = count($nums);
+        
+        for ($i=0;$i <= $array_size;$i++) {
+            for ($j=0;$j <= $array_size;$j++) {
+                if ($nums[$i] + $nums[$j] === $target) {
+                    return [
+                        $i,
+                        $j 
+                    ];
+                }
+            }
+        }
+    }
+}
