7d00e2f6a270f4ed0ee2b0fb0f9f003650dfd37b
William Trindade <williamtrindade777@gmail.com>
2020-01-22 00:03:37 -0300

    Update index.php

--- #1-two-sum/index.php
+++ #1-two-sum/index.php
@@ -8,13 +8,11 @@ class Solution {
      */
     function twoSum($nums, $target) {
         $array_size = count($nums);
-        
-        for ($i=0;$i <= $array_size;$i++) {
-            for ($j=0;$j <= $array_size;$j++) {
-                if ($nums[$i] + $nums[$j] === $target) {
+        for ($i=0;$i < $array_size;$i++) {
+            for ($j=0;$j < $array_size;$j++) {
+                if (($nums[$i] + $nums[$j] == $target) && ($i != $j) ) {
                     return [
-                        $i,
-                        $j 
+                        $i, $j 
                     ];
                 }
             }
