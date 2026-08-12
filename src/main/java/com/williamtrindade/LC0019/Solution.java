c1d31c84f342aa8cec85dbb00d00892d4989dc54
William Trindade <williamtrindade777@gmail.com>
2026-08-12 11:14:58 -0300

    Add method to remove nth node from end of list
    Implement removeNthFromEnd method to remove the nth node from the end of a linked list.

---
+++ src/main/java/com/williamtrindade/LC0019/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 1. Cria um nó falso que aponta para o head
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode slow = dummy; // Este é o seu nodeI
+        ListNode fast = dummy; // Este é o seu nodeJ
+        
+        // 2. Avança o fast 'n + 1' passos à frente
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        
+        // 3. Move os dois juntos até o fast passar do último nó da lista
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        // 4. O slow agora está EXATAMENTE um nó antes do alvo; fazemos a ponte
+        slow.next = slow.next.next;
+        
+        // Retorna dummy.next, o que resolve o problema caso o head original tenha sido deletado
+        return dummy.next;
+    }
+}
